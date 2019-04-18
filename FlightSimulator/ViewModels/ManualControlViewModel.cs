e73a21ce908dbc6271c9341e4412f295d200c4d0
idant <idant@LAPTOP-1CBL0P6J>
2019-04-18 18:59:53 +0300

    asd

--- FlightSimulator/ViewModels/ManualControlViewModel.cs
+++ FlightSimulator/ViewModels/ManualControlViewModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using FlightSimulator.ViewModels;
 
 namespace FlightSimulator.ViewModels
 {
@@ -24,6 +26,7 @@ namespace FlightSimulator.ViewModels
         {
             set
             {
+                new Thread(() => Commands.CommandsInstance.sendCommands
 
             }
             get
